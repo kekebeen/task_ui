b57a85fbd68e428ba69079f98691ea99cc9d3282
kekebeen <kakosi69@gmail.com>
2017-02-21 11:54:59 -0500

    Added range slider

--- src/components/PriceRange/PriceRange.js
+++ src/components/PriceRange/PriceRange.js
@@ -8,19 +8,30 @@ let PriceRange = React.createClass({
     }
   },
   change: function (e) {
-    console.log("change",e)
+    this.setState({
+      value: {
+        min: e[0],
+        max: e[1]
+      }
+    })
   },
   render() {
     return (
       <div className='collapsible__item collapsible__item--range'>
         <h1 className='price__title'>{this.props.title}:</h1>
-        <em>Min</em>
-        <input type="number" />
-        <em>Max</em>
-        <input type="number" />
+        <div className="input__group">
+          <div className="input__control">
+            <div className="label">From</div>
+            <input type="text" ref="numberMin" name="numberMin" id="numberMin" value={this.state.value.min}/>
+          </div>
+          <div className="input__control">
+            <div className="label">To</div>
+            <input type="text" ref="numberMax" name="numberMax" id="numberMax" value={this.state.value.max}/>
+          </div>
+        </div>
 
         <div className='range__slider'>
-        <ReactSlider defaultValue={[20, 80]} withBars onChange={this.change} disabled>
+        <ReactSlider defaultValue={[20, 80]} withBars onChange={this.change} pearling={true} snapDragDisabled={true}>
           <div className="my-handle">1</div>
           <div className="my-handle">2</div>
         </ReactSlider>,
